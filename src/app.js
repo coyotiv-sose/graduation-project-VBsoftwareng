aa58ada0f23aa2ba083a11b86183bda5a7dcd634
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2023-12-19 13:46:29 +0000

    Add new routes for appointment, doctor, medical records

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan')
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var patientsRouter = require('./routes/patients.js')
+var doctorsRouter = require('./routes/doctors.js')
+var appointmentsRouter = require('./routes/appointments.js')
+var medicalRecordsRouter = require('./routes/medicalRecords.js')
 
 var app = express()
 
@@ -23,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/patients', patientsRouter)
+app.use('/doctors', doctorsRouter)
+app.use('/appointments', appointmentsRouter)
+app.use('/medicalRecords', medicalRecordsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- src/client.js
+++ src/client.js
@@ -10,10 +10,54 @@ async function getUsers() {
   try {
     const response = await client.get('/users')
 
-    console.log(response)
+    console.log(response.data)
   } catch (error) {
     console.error(error)
   }
 }
 
 getUsers()
+
+async function getPatients() {
+  try {
+    const response = await client.get('/patients')
+
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+getPatients()
+
+async function getDoctors() {
+  try {
+    const response = await client.get('/doctors')
+
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+getDoctors()
+
+async function getAppointments() {
+  try {
+    const response = await client.get('/appointments')
+
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+getAppointments()
+
+async function getMedicalRecords() {
+  try {
+    const response = await client.get('/medicalRecords')
+
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+getMedicalRecords()
