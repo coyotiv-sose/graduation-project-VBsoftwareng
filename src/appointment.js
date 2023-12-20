2376b86fbef9939cf9be416b09f8ffca0fc7909c
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2023-12-20 12:25:15 +0000

    added address to appointment

--- src/appointment.js
+++ src/appointment.js
@@ -1,10 +1,11 @@
 class Appointment {
-  constructor(doctor, patient, date, time, location) {
+  constructor(doctor, patient, date, time, location, address) {
     this.doctor = doctor
     this.patient = patient
     this.date = date
     this.time = time
     this.location = location
+    this.address = address
     this.reviewText = undefined
     this.ratingNumber = undefined
   }
