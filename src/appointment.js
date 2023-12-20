8508a21f73a491de6f587a715b034413bd37a99b
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2023-12-20 11:25:52 +0000

    add location to appointment

--- src/appointment.js
+++ src/appointment.js
@@ -1,9 +1,10 @@
 class Appointment {
-  constructor(doctor, patient, date, time) {
+  constructor(doctor, patient, date, time, location) {
     this.doctor = doctor
     this.patient = patient
     this.date = date
     this.time = time
+    this.location = location
     this.reviewText = undefined
     this.ratingNumber = undefined
   }
