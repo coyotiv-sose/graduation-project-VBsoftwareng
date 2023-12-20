238dbcdf6ef11f87ec903f7f1e4d8e4be1e35690
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2023-12-20 09:13:21 +0000

    add post to create new Doctor

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -1,9 +1,18 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const Doctor = require('../doctor')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send({ title: 'SiDoctor' })
+  res.send({ title: 'SiDoctor yo soy el Doctor' })
 })
 
+// Create a new Doctor using post
+router.post('/', function (req, res, next) {
+  const { name, lastName, especialization, location, address } = req.body
+  // create a new doctor
+  const newDoctor = new Doctor(name, lastName, especialization, location, address)
+  // send back that doctor
+  res.send(newDoctor)
+})
 module.exports = router
