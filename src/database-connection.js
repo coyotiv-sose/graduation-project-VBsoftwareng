89b56b278efbd2ee43c3617f34a88fcc6feecd2b
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-01-11 08:26:11 +0000

    Set up a local database and 2 containers so we dont have to refresh everytime

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
 
-const Cat = mongoose.model('Cat', { name: String })
+// const username = process.env.MONGODB_USERNAME
+// const password = process.env.MONGODB_PASSWORD
+// const dbName = process.env.MONGODB_DATABASE
+// const port = process.env.MONGODB_PORT
+//let connectionString = process.env.MONGODB_CONNECTION_STRING
 
-const kitty = new Cat({ name: 'Zildjian' })
-kitty.save().then(() => console.log('meow'))
+// if (!connectionString){
+//   connectionString = mongo
+// }
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => console.log('conected to mongodb'))
+
+// const Cat = mongoose.model('Cat', { name: String })
+
+// // const connectionString = process.env.MONGODB_REMOTE
+// //   ? `mongodb+srv://${username}:${password}@${dbName}?retryWrites=true&w=majority`
+// //   : `mongodb://localhost:27017/${dbName}`
+
+// // mongoose.connect(conecctionString, {})
+
+// const kitty = new Cat({ name: 'Zildjian' })
+// kitty.save().then(() => console.log('meow'))
