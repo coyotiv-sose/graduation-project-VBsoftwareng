834a6bd1141c55a885011225cf95c0b72067aa52
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-01-17 09:13:48 +0000

    Add composition API and Options examples so I can copy it after and adjust it

---
+++ frontend/src/components/CounterCompositionAPI.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+// for (let i = 0; i < 100000; i++) {
+//   console.log(i)
+// }
+const increment = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+  <button @click="increment">Composition API Count is: {{ count }}</button>
+</template>
+
+<style scoped>
---
+++ frontend/src/components/icons/CounterOptionsAPI.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  mounted() {
+    this.count = 10
+    console.log('mounted')
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    wait() {
+      for (let i = 0; i < 100000; i++) {
+        console.log(i)
+      }
+    },
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+<template>
+  <button @click="increment">Options API Count is: {{ count }}</button>
+</template>
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
