127e6295b228010bd7d9d32fd26bfc6d729d91c8
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-01-17 09:11:54 +0000

    Retriving doctors at startup using monted

---
+++ frontend/src/views/DoctorsView.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from 'axios'
+export default {
+  name: 'DoctorsView',
+  components: {},
+  data() {
+    return {
+      doctors: []
+    }
+  },
+  methods: {
+    async fetchDoctors() {
+      const doctors = (await axios.get('http://localhost:3000/doctors')).data
+
+      this.doctors = doctors
+    }
+  },
+  mounted() {
+    this.fetchDoctors()
+  }
+}
+</script>
+<template>
+  <main>
+    <h1>Doctors</h1>
+    <ul>
+      <li v-for="doctor in doctors" :key="doctor._id">
+        <p>{{ doctor.name }}</p>
+      </li>
+    </ul>
+  </main>
+</template>
