ef47a6e413102b966284e5cdce0ed3a2c4ca3d82
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-01-25 15:38:29 +0000

    Added name and last name deleted nickName since I dont use it for the authUser

--- backend/src/model/authUser.js
+++ backend/src/model/authUser.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const authUserSchema = new mongoose.Schema({
-  nickName: {
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
     type: String,
     required: true,
   },
