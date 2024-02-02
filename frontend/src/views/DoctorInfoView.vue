c60195c9939330e0d15db67bedbe67923fd133c1
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-02-02 20:01:11 +0000

    Add athe calendar (needs to be reviewed)

--- frontend/src/views/DoctorInfoView.vue
+++ frontend/src/views/DoctorInfoView.vue
@@ -1,13 +1,28 @@
 <script>
 import { useDoctorStore } from '@/stores/doctor'
 import { mapActions } from 'pinia'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 export default {
   name: 'doctorInfoView',
+  components: {
+    FullCalendar
+  },
   data() {
     return {
       doctor: null,
-      isLoading: false
+      isLoading: false,
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        selectable: true,
+        events: [
+          { title: 'event 1', date: '2024-02-03' },
+          { title: 'event 2', date: '2024-03-03' }
+        ]
+      }
     }
   },
   methods: {
@@ -26,5 +41,6 @@ export default {
   <main v-if="!isLoading">
     <h1>Doctor</h1>
     <p>{{ doctor.name }} {{ doctor.lastName }}</p>
+    <FullCalendar :options="calendarOptions" />
   </main>
 </template>
