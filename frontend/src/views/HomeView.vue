6ee5506a9325e6675bdb242ec5319fa41edb2307
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-02-03 15:33:29 +0000

    Change home page information

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,6 +3,11 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
+  <h1>Welcome to SiDoctor!</h1>
+  <h2>The easy way to book an appointment with the doctor you need</h2>
+  <br />
+  <br />
+  <h3>Learn everything about improving your health</h3>
   <main>
     <Suspense>
       <TheWelcome />
