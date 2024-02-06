c90e850e6f944e435712d41171a57978cbec7353
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-02-06 10:37:24 +0000

    Set up initial doctor calendar (not appearing)

--- frontend/src/views/DoctorInfoView.vue
+++ frontend/src/views/DoctorInfoView.vue
@@ -1,37 +1,126 @@
 <script>
 import { useDoctorStore } from '@/stores/doctor'
 import { mapActions } from 'pinia'
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import VueMeetingSelector from 'vue-meeting-selector'
+import 'vue-meeting-selector/dist/style.css'
+// Function used to generate slots, use your own function
+import SlotsGenerator from '@/components/slotsGenerator'
+
+// import FullCalendar from '@fullcalendar/vue3'
+// import dayGridPlugin from '@fullcalendar/daygrid'
+// import interactionPlugin from '@fullcalendar/interaction'
 
 export default {
   name: 'doctorInfoView',
   components: {
-    FullCalendar
+    // FullCalendar
+    VueMeetingSelector
   },
   data() {
     return {
       doctor: null,
       isLoading: false,
-      calendarOptions: {
-        plugins: [dayGridPlugin, interactionPlugin],
-        initialView: 'dayGridMonth',
-        selectable: true,
-        events: [
-          { title: 'event 1', date: '2024-02-03' },
-          { title: 'event 2', date: '2024-03-03' }
-        ]
+      meeting: null,
+      meetingsDays: [],
+      date: new Date(),
+      loading: true
+
+      // calendarOptions: {
+      //   plugins: [dayGridPlugin, interactionPlugin],
+      //   initialView: 'dayGridMonth',
+      //   selectable: true,
+      //   events: [
+      //     { title: 'event 1', date: '2024-02-03' },
+      //     { title: 'event 2', date: '2024-03-03' }
+      //   ]
+      // }
+    }
+  },
+  computed: {
+    nbDaysToDisplay() {
+      return 5
+    },
+    classNames() {
+      return {
+        tabLoading: 'loading-div'
       }
     }
   },
   methods: {
-    ...mapActions(useDoctorStore, ['fetchDoctor'])
+    ...mapActions(useDoctorStore, ['fetchDoctor']),
+    SlotsGeneratorAsync(
+      // date
+      d,
+      // nbDaysToDisplay
+      n,
+      start,
+      end,
+      timesBetween
+    ) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(SlotsGenerator(d, n, start, end, timesBetween))
+        }, 1000)
+      })
+    },
+    async nextDate() {
+      this.loading = true
+      const start = {
+        hours: 8,
+        minutes: 0
+      }
+      const end = {
+        hours: 16,
+        minutes: 0
+      }
+      const dateCopy = new Date(this.date)
+      const newDate = new Date(dateCopy.setDate(dateCopy.getDate() + 7))
+      this.date = newDate
+      this.meetingsDays = await SlotsGeneratorAsync(newDate, this.nbDaysToDisplay, start, end, 30)
+      this.loading = false
+    },
+    async previousDate() {
+      this.loading = true
+      const start = {
+        hours: 8,
+        minutes: 0
+      }
+      const end = {
+        hours: 16,
+        minutes: 0
+      }
+      const dateCopy = new Date(this.date)
+      dateCopy.setDate(dateCopy.getDate() - 7)
+      const formatingDate = (dateToFormat) => {
+        const d = new Date(dateToFormat)
+        const day = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate()
+        const month = d.getMonth() + 1 < 10 ? `0${d.getMonth() + 1}` : d.getMonth() + 1
+        const year = d.getFullYear()
+        return `${year}-${month}-${day}`
+      }
+      const newDate =
+        formatingDate(new Date()) >= formatingDate(dateCopy) ? new Date() : new Date(dateCopy)
+      this.date = newDate
+      this.meetingsDays = await SlotsGeneratorAsync(newDate, this.nbDaysToDisplay, start, end, 30)
+      this.loading = false
+    }
   },
   async created() {
     this.isLoading = true
     this.doctor = await this.fetchDoctor(this.$route.params.doctorId)
     this.isLoading = false
+  },
+  async mounted() {
+    const start = {
+      hours: 8,
+      minutes: 0
+    }
+    const end = {
+      hours: 16,
+      minutes: 0
+    }
+    this.meetingsDays = await SlotsGeneratorAsync(this.date, this.nbDaysToDisplay, start, end, 30)
+    this.loading = false
   }
 }
 </script>
@@ -41,6 +130,33 @@ export default {
   <main v-if="!isLoading">
     <h1>Doctor</h1>
     <p>{{ doctor.name }} {{ doctor.lastName }}</p>
-    <FullCalendar :options="calendarOptions" />
+
+    <div class="simple-example">
+      <vue-meeting-selector
+        class="simple-example__meeting-selector"
+        v-model="meeting"
+        :date="date"
+        :loading="loading"
+        :class-names="classNames"
+        :meetings-days="meetingsDays"
+        @next-date="nextDate"
+        @previous-date="previousDate"
+      />
+      <p>meeting Selected: {{ meeting ? meeting : 'No Meeting selected' }}</p>
+    </div>
   </main>
 </template>
+<style scoped lang="scss">
+.simple-example {
+  &__meeting-selector {
+    max-width: 542px;
+  }
+}
+// since our scss is scoped we need to use ::v-deep
+:deep(.loading-div) {
+  top: 58px !important;
+  meeting-selector {
+    top: 58px !important;
+  }
+}
+</style>
