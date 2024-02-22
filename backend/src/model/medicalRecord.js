b942db2a875705f23c1264fd9e9aedbe770fea5b
VBsoftwareng <150044232+VBsoftwareng@users.noreply.github.com>
2024-02-22 11:55:10 +0000

    Refactor medicalRecord model and add timestamps and mongoose

--- backend/src/model/medicalRecord.js
+++ backend/src/model/medicalRecord.js
@@ -1,8 +1,31 @@
-class MedicalRecord {
-  constructor(patient, diagnosis, prescription) {
-    this.patient = patient.name
-    this.diagnosis = diagnosis
-    this.prescription = prescription
+const mongoose = require('mongoose')
+
+const medicalRecordSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Patient',
+      autopopulate: true,
+    },
+    diagnosis: {
+      type: String,
+      required: true,
+    },
+    prescription: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
   }
-}
-module.exports = MedicalRecord
+)
+
+medicalRecordSchema.plugin(require('mongoose-autopopulate'))
+module.exports = mongoose.model('MedicalRecord', medicalRecordSchema)
+
+//convert to mongoose
+// update models doctor and patient
+//update routes handlers mediacl record creation
+
+//update
